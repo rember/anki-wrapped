abd8e75a47a86639ed950fac14b8ed4ef1734dc9
Giacomo Randazzo <giacomoran@gmail.com>
2024-12-01 22:45:25 +0100

    add SSRed page

---
+++ src/routes/anki-wrapped-2024/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	// Generate a random number between 0 and 9999
+	let randomNumber = Math.floor(Math.random() * 10000);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>This is ssr</p>
+<p>Random Number: {randomNumber}</p>
