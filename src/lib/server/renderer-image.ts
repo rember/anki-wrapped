a367c579004d6a3489ec381cd5da7dfefee3054a
Giacomo Randazzo <giacomoran@gmail.com>
2024-12-02 10:11:12 +0100

    more attempts

--- src/lib/server/renderer-image.ts
+++ src/lib/server/renderer-image.ts
@@ -1,5 +1,10 @@
-import { initWasm as initWasmResvg, Resvg } from '@resvg/resvg-wasm';
+import { Resvg, initWasm as initWasmResvg } from '@resvg/resvg-wasm';
+
 import { Effect } from 'effect';
+
+// @ts-expect-error
+import URL_WASM_RESVG from '@resvg/resvg-wasm/index_bg.wasm?module';
+
 import type { DataImage } from '../shared/values';
 
 // #:
@@ -7,14 +12,14 @@ import type { DataImage } from '../shared/values';
 export class RendererImage extends Effect.Service<RendererImage>()('RendererImage', {
 	effect: Effect.gen(function* () {
 		// ##: Init Resvg WASM
-		// TODO: Load Resvg correctly instead of from a CDN.
 
-		yield* Effect.promise(() => initWasmResvg('https://unpkg.com/@resvg/resvg-wasm/index_bg.wasm'));
+		// const URL_WASM_RESVG = yield* Effect.promise(() => import('@resvg/resvg-wasm/index_bg.wasm'));
+		yield* Effect.promise(() => initWasmResvg(URL_WASM_RESVG));
 
 		// ##: renderPng
 
 		const renderPng = (args: { dataImage: DataImage; svg: string }) =>
-			Effect.sync(() => {
+			Effect.gen(function* () {
 				const renderer = new Resvg(args.svg, {
 					background: 'white',
 					font: { loadSystemFonts: false }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,81 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+// import wasmEsm from 'vite-plugin-wasm-esm';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
+// #:
+// REFS: https://github.com/patdx/pmil.me/blob/f21bb110fae51fb0679e4b83d4d2e2571d09b7f6/vite.config.ts#L121
 
+function makeWasmLoader(wasmPath: string) {
+	const code = /* js */ `import fs from "fs";
+
+const wasmModule = new WebAssembly.Module(fs.readFileSync(${JSON.stringify(wasmPath)}));
+export default wasmModule;
+`;
+	return code;
+}
+
+const cloudflareStyleWasmLoader = () => {
+	let isDev = false;
+
+	return {
+		name: 'cloudflare-style-wasm-loader',
+		enforce: 'pre',
+		config(config, env) {
+			return {
+				build: { rollupOptions: { external: [/.+\.wasm$/i] } }
+			};
+		},
+		configResolved(config) {
+			isDev = config.command === 'serve';
+		},
+		resolveId(id) {
+			if (isDev) return;
+			// prod only
+
+			if (id.endsWith('.wasm?module')) {
+				console.log('Resolving WASM file:', id);
+				return {
+					id: id.slice(0, -1 * '?module'.length),
+					external: true
+				};
+			}
+		},
+		load(id) {
+			if (!isDev) return;
+			// dev only
+
+			if (id.endsWith('.wasm?module')) {
+				const actualId = id.slice(0, -1 * '?module'.length);
+				console.log('Loading WASM file:', id);
+				return makeWasmLoader(actualId);
+			}
+		}
+	} satisfies Plugin;
+};
+
+// #:
+
+export default defineConfig({
+	plugins: [topLevelAwait(), cloudflareStyleWasmLoader(), sveltekit()],
+	// assetsInclude: ['**/*.wasm'],
+	// optimizeDeps: {
+	// 	exclude: ['@cf-wasm/resvg']
+	// },
+	// ssr: {
+	// 	external: ['@cf-wasm/resvg']
+	// },
+	// build: {
+	// 	rollupOptions: {
+	// 		external: ['@cf-wasm/resvg']
+	// 	}
+	// },
+	optimizeDeps: {
+		exclude: ['@resvg/resvg-wasm/index_bg.wasm']
+	},
+	ssr: {
+		external: ['@resvg/resvg-wasm/index_bg.wasm']
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
